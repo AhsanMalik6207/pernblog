40250250e2292dc9fa7943c516a1f6a58bcb6ea5
Muttarab Ahmad <muttarabahmad9@gmail.com>
2022-01-18 15:52:27 +0500

    Revert "Revert "final_commit""

---
+++ server/migrations/20220110205835-create-comment.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      comment_text: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      postId: {
+        onDelete: 'CASCADE',
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Comments');
+  }
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,12 @@
 const express = require("express"),
   router = express.Router();
+<<<<<<< HEAD
+const Controller = require('../controllers/user');
+=======
 const Controller = require("../controllers/user");
 
 router.post("/register", Controller.register);
 router.post("/login",Controller.login);
 router.post("/renewaccesstoken",Controller.renewaccesstoken);
 module.exports = router;
+>>>>>>> master
