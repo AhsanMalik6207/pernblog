01603546e329b023f071c817d9ed2e30b49133a2
Muttarab Ahmad <muttarabahmad9@gmail.com>
2022-02-16 03:07:56 +0500

    15february2022

--- server/controllers/like.js
+++ server/controllers/like.js
@@ -1,26 +1,29 @@
 const Like = require("../models/like");
+
+exports.getLikebyPost = async function (request, response) {
+  try {
+    const likebypost = await Like.findAll({ where: { postId: request.params.id } });
+    response.status(200).json(likebypost);
+  } catch (error) {
+    response.status(500).json(error)
+  }
+}
+exports.getLikebyUser = async function (request, response) {
+  try {
+    const likebyuser = await Like.findAll({ where: { userId: request.params.userid,postId:request.params.postid } });
+    response.status(200).json(likebyuser);
+  } catch (error) {
+    response.status(500).json(error)
+  }
+}
+
 exports.like = async function (req, res) {
   try {
-    const { postId,userId} = req.params
-    return Like
-      .create({
-        postId,
-        userId
-      })
-      .then(like => res.status(201).send({
-        message: `Your have liked the post`,
-        like
-      }))
-  } catch (e) {
-    return res.status(400).json({ status: 400, message: e.message });
-  }
-};
-
-
-exports.unlike = async function (req, res) {
-  try {
-    return Like
-          .findByPk(req.params.likeId)
+    Like.findOne({ where: { postId: req.params.postId,userId:req.params.userId} }).then(like => {
+      if (like !== null) {
+        const likeId = like.id;
+        return Like
+          .findByPk(likeId)
           .then(like => {
             return like
               .destroy()
@@ -30,6 +33,19 @@ exports.unlike = async function (req, res) {
               .catch(error => res.status(400).send(error));
           })
           .catch(error => res.status(400).send(error))
+      } else {
+        const { postId, userId } = req.params
+        return Like
+          .create({
+            postId,
+            userId
+          })
+          .then(like => res.status(201).send({
+            message: `Your have liked the post`,
+            like
+          }))
+      }
+    })
   } catch (e) {
     return res.status(400).json({ status: 400, message: e.message });
   }
--- server/routes/like.js
+++ server/routes/like.js
@@ -3,6 +3,7 @@ const express = require("express"),
 const Controller = require("../controllers/like");
 const checkAuthMiddleware = require('../middleware/check-auth');
 
-router.post("/:userId/:postId/like", checkAuthMiddleware.checkAuth,Controller.like);
-router.delete("/:likeId/unlike", checkAuthMiddleware.checkAuth,Controller.unlike);
+router.post("/:userId/:postId",Controller.like);
+router.get("/:id",Controller.getLikebyPost)
+router.get("/:userid/:postid",Controller.getLikebyUser)
 module.exports = router;
