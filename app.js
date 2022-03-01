4836dd6021aab646f6dad41b1d485b684ff9e579
Muttarab Ahmad <muttarabahmad9@gmail.com>
2022-03-01 14:52:43 +0500

    01/03/2022

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false, limit: '5mb' }));
 app.use(bodyParser.json());
 app.use("/", routes);
-app.use(express.static('./images'))
+app.use(express.static('./images'));
 
 const server = app.listen(8000, "localhost", function () {
   console.log(`Blog App listening at http://localhost:${8000}`);
