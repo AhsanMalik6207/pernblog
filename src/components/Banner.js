6fd70ff1aaa8e99615fe02bf6c2c1c7a78ab1740
Muttarab Ahmad <muttarabahmad9@gmail.com>
2022-03-14 15:13:04 +0500

    14/03/2022

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import { makeStyles, Box, Typography } from '@material-ui/core';
 const useStyle = makeStyles({
     image: {
         width: '100%',
-        background: `url(${'https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg'}) center/55% repeat-x #000`,
+        background: `url(${'https://images.pexels.com/photos/1714208/pexels-photo-1714208.jpeg'}) center/100% repeat-x #000`,
         height: '50vh',
         display: 'flex',
         flexDirection: 'column',
